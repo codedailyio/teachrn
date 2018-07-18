3e65d91ad58466d63629174def949d9e390a1531
Jason Brown <browniefed@gmail.com>
2018-07-17 19:00:36 -0700

    Initial

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "#000",
+    width: 200,
+    height: 200,
+  },
+});
