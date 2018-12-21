4dbc775198f33b4bfbfae6374d7a467637ec3399
Jason Brown <browniefed@gmail.com>
2018-12-21 11:23:48 -0800

    Add in popin bottom sheet

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Animated, Dimensions } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0),
+  };
+  handleOpen = () => {
+    Animated.spring(this.state.animation, {
+      toValue: 1,
+      overshootClamping: true,
+    }).start();
+  };
+  handleClose = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 0,
+      duration: 200,
+    }).start();
+  };
   render() {
+    const backdrop = {
+      transform: [
+        {
+          translateY: this.state.animation.interpolate({
+            inputRange: [0, 0.01],
+            outputRange: [Dimensions.get("window").height, 0],
+            extrapolate: "clamp",
+          }),
+        },
+      ],
+      opacity: this.state.animation.interpolate({
+        inputRange: [0.01, 0.5],
+        outputRange: [0, 1],
+        extrapolate: "clamp",
+      }),
+    };
+
+    const fullUp = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, -1 * Dimensions.get("window").height],
+    });
+
+    const translate = {
+      transform: [{ translateY: fullUp }],
+    };
+
     return (
       <View style={styles.container}>
+        <TouchableOpacity onPress={this.handleOpen}>
+          <Text>Open</Text>
+        </TouchableOpacity>
 
+        <Animated.View style={[StyleSheet.absoluteFill, styles.cover, backdrop]}>
+          <Animated.View style={[styles.sheet]}>
+            <Animated.View style={[styles.popup, translate]}>
+              <TouchableOpacity onPress={this.handleClose}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </Animated.View>
+        </Animated.View>
       </View>
     );
   }
@@ -17,9 +70,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  box: {
-    backgroundColor: "#000",
-    width: 200,
-    height: 200,
+  cover: {
+    backgroundColor: "rgba(0,0,0,.5)",
+  },
+  sheet: {
+    position: "absolute",
+    top: Dimensions.get("window").height,
+    left: 0,
+    right: 0,
+    height: "100%",
+    justifyContent: "flex-end",
+    flex: 1,
+  },
+  popup: {
+    backgroundColor: "#FFF",
+    marginHorizontal: 10,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    minHeight: 75,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
