2054ac224ef8ff28954d474eefef21a303a36eb5
Jason Brown <browniefed@gmail.com>
2018-12-23 15:21:43 -0800

    Add code

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Animated } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    animation: new Animated.Value(1),
+  };
+  componentDidMount = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 0,
+      duration: 15000,
+    }).start();
+  };
+
   render() {
+    const animation = {
+      right: this.state.animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["100%", "0%"],
+      }),
+      // transform: [
+      //   {
+      //     translateX: this.state.animation.interpolate({
+      //       inputRange: [0, 1],
+      //       outputRange: [-96, 0],
+      //     }),
+      //   },
+      // ],
+    };
+
     return (
       <View style={styles.container}>
-
+        <View style={styles.pill}>
+          <Animated.View style={[StyleSheet.absoluteFill, styles.fill, animation]} />
+          <View style={styles.wrap}>
+            <View style={[StyleSheet.absoluteFill, styles.content]}>
+              <View style={styles.img}>
+                <Text style={styles.text}>JB</Text>
+              </View>
+              <View>
+                <Text style={styles.text}>$100.00</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -17,9 +54,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  box: {
-    backgroundColor: "#000",
-    width: 200,
-    height: 200,
+  pill: {
+    width: 96,
+    backgroundColor: "#48aa77",
+    borderRadius: 48,
+    overflow: "hidden",
+    transform: [{ scale: 2 }],
+  },
+  fill: {
+    backgroundColor: "#25573d",
+  },
+  wrap: {
+    height: 28,
+    width: '100%',
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 4
+  },
+  img: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: "tomato",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 14,
   },
 });
