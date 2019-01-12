09711b6ec19ab1072b42c1120fbc2818fd6d1c6a
Jason Brown <browniefed@gmail.com>
2019-01-11 20:31:50 -0800

    Add tracking code

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, Animated, View, TouchableOpacity } from "react-native";
 
-export default class App extends React.Component {
+const items = [0, 1, 2, 3, 4, 5, 6, 7];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class App extends Component {
+  state = {
+    index: new Animated.Value(0),
+    track: new Animated.Value(0),
+  };
+  componentDidMount = () => {
+    Animated.timing(this.state.index, {
+      duration: 500,
+      toValue: this.state.track,
+    }).start();
+  };
+
   render() {
+    const { index } = this.state;
+
     return (
       <View style={styles.container}>
-
+        <View style={styles.background}>
+          {items.map(i => {
+            const translateX = Animated.multiply(Animated.subtract(index, i), 40);
+            const transform = {
+              transform: [{ translateX }],
+            };
+            return (
+              <TouchableOpacity
+                style={styles.circle}
+                key={i}
+                onPress={() => this.state.track.setValue(i)}
+              >
+                <Animated.View style={[styles.mover, transform]} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </View>
     );
   }
@@ -17,9 +52,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  box: {
-    backgroundColor: "#000",
-    width: 200,
-    height: 200,
+  background: {
+    flexDirection: "row",
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#ddd",
+    borderRadius: 20,
+    marginRight: 4,
+    overflow: "hidden",
+  },
+  mover: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "tomato",
   },
 });
+
+export default App;
